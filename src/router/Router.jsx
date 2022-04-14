4f7a1127341049776e87f80d777490672e77efc2
riki-yajima <94480246+riki-yajima@users.noreply.github.com>
2022-04-14 15:44:02 +0900

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Top } from "../components/pages/Top";
+import { Users } from "../components/pages/Users";
+import { DefaultLayout } from "../components/templates/DefaultLayout";
+import { HeaderOnly } from "../components/templates/HeaderOnly";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <DefaultLayout />
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
